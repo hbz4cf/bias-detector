bb23c1e699ce09a96f6511291e645a354234b176
hbz4cf <hbz4cf@virginia.edu>
2025-12-01 21:00:57 -0500

    Simplify main app.run configuration
    Removed redundant port configuration for app.run.

--- src/api.py
+++ src/api.py
@@ -34,7 +34,3 @@ def predict():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # default to 8080 locally
-    app.run(host="0.0.0.0", port=port)
